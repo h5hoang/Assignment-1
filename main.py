4e7e32b26791295250a95b47c8f394ed61f909d0
h5hoang <hanson.hoang@ucsf.edu>
2024-10-17 19:08:18 -0700

    more documentation
    documentation

--- main.py
+++ main.py
@@ -9,30 +9,41 @@ def main():
     TODO: The main function
     """
     # load in fasta and fastq files
+    # Create instance of FastaParser
     aparser = FastaParser('data/test.fa')
+    # Create instance of FastqParser
     qparser = FastqParser('data/test.fq')    
     
+    # For each record of FastaParser, Transcribe the sequence
+    # and print it to console
     # prints header and the sequence for every sequence in test.fa using the transcribe function
-    print("\napplying ranscribe function on test.fa:")
+    print("\napplying transcribe function on test.fa:")
     for header, sequence in aparser:
+        #print(transcribe(sequence))
         rna_sequence = transcribe(sequence)
         print(f"{header}: {rna_sequence}")
 
+    # For each record of FastqParser, Transcribe the sequence
+    # and print it to console
     # prints header and the sequence for every sequence in test.fq using the transcribe function
-    print("\napplying ranscribe function on test.fq:")
+    print("\napplying transcribe function on test.fq:")
     for header, sequence, quality in qparser:
         rna_sequence = transcribe(sequence)
         print(f"{header}: {rna_sequence}, {quality}")
 
 
+    # For each record of FastaParser, Reverse Transcribe the sequence
+    # and print it to console
     # prints header and the sequence for every sequence in test.fa using the reverse transcribe function
-    print("\napplying reverse ranscribe function on test.fa:")
+    print("\napplying reverse transcribe  function on test.fa:")
     for header, sequence in aparser:
         reverse_rna_sequence = reverse_transcribe(sequence)
         print(f"{header}: {reverse_rna_sequence}")
 
+    # For each record of FastqParser, Reverse Transcribe the sequence
+    # and print it to console
     # prints header and the sequence for every sequence in test.fq using the reverse transcribe function
-    print("\napplying reverse ranscribe function on test.fq:")
+    print("\napplying reverse transcribe function on test.fq:")
     for header, sequence, quality in qparser:
         reverse_rna_sequence = reverse_transcribe(sequence)
         print(f"{header}: {reverse_rna_sequence}, {quality}")
