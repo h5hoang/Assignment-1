5232d858fc2024edd81ec666d4b6591feff6ceff
h5hoang <hanson.hoang@ucsf.edu>
2024-10-18 11:49:46 -0700

    more tests
    tests for spaces, fixed some tests

--- tests/test_transcribe.py
+++ tests/test_transcribe.py
@@ -40,7 +40,7 @@ def test_transcribe():
     assert transcribe("") == ""  
 
     # test lower case dna inputs
-    assert transcribe("aagtc") == "TTCAG"
+    assert transcribe("aagtc   ") == "UUCAG"
 
     # TESTING FOR INVALID INPUT
     # test if my transcribe function raises an error when there is an invalid nucleotide in the input
@@ -70,8 +70,8 @@ def test_reverse_transcribe():
     # test blank strings
     assert reverse_transcribe("") == ""  
 
-    # test lower case dna inputs
-    assert transcribe("aagtc") == "GACUU"
+    # test lower case dna inputs with spaces
+    assert reverse_transcribe("   aagtc  ") == "GACUU"
 
     # TESTING FOR INVALID INPUT
     # test if my reverse_transcribe function raises an error (from the transcribe function) when there is an invalid nucleotide in the input
